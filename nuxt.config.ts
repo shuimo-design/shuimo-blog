4cdc89fa5a86a3817ca76785c00198dc362b048b
你好怪凡 <higuaifan@higuaifan.com>
2024-02-27 01:25:12 +0800

    🧩 use shuimo-ui-nuxt instead of modules/shuimo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,27 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@shuimo-design/shuimo-ui-nuxt'
   ],
   css: [
     '~/assets/styles/index.css',
-    '~/plugins/editor/style/editor.css'
+    '~/plugins/editor/style/editor.css',
   ],
   runtimeConfig: {
     public: {
-      NUXT_ENV_SERVER: process.env.NUXT_ENV_SERVER
-    }
+      NUXT_ENV_SERVER: process.env.NUXT_ENV_SERVER,
+    },
+  },
+  build: {
+    transpile: ['shuimo-ui'],
   },
   app: {
     layoutTransition: {
-      name: 'm-layout', mode: 'default'
+      name: 'm-layout', mode: 'default',
     },
     pageTransition: {
-      name: 'm-page', mode: 'out-in'
-    }
-  }
+      name: 'm-page', mode: 'out-in',
+    },
+  },
 });
